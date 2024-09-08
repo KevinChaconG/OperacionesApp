e17f88be66ba22382e20a0f1eddedc512b552576
Kevin Chacon <kevin27chacon@gmail.com>
2024-09-07 19:04:36 -0600

    Add project files.

---
+++ OperacionesApp/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace OperacionesApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new TabbedPage
+            {
+                Children =
+                {
+                    new CuadradoPage {Title = "Area Cuadrado"},
+                    new CirculoPage { Title = "Area Circulo"},
+                    new RectanguloPage { Title = "Area Rectangulo"}
+                }
+            };
+        }
+    }
+}
